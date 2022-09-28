291137863d70fc9dbc306078d2b1b58e99a85f99
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-09-28 14:38:41 -0300

    Feat: Authorization repository created

---
+++ src/repositories/authRepository.ts
@@ -0,0 +1,22 @@
+import { Prisma } from "@prisma/client";
+import { prisma } from "../config/database";
+import { TypeNewUserData } from "../types/userTypes";
+
+export async function checkEmail(email: string) {
+  const result = await prisma.user.findFirst({ where: { email } });
+  return result;
+};
+
+export async function checkUsername(username: string) {
+  const result = await prisma.user.findFirst({ where: { username } });
+  return result;
+};
+
+export async function createUser(user: TypeNewUserData) {
+  await prisma.user.create({ data: user });
+};
+
+export async function findById(id: number) {
+  const result = await prisma.user.findFirst({ where: { id } });
+  return result;
+};
