dc008a63f5e74b901bb191bec3d4bfbe16445add
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-09-28 11:26:21 -0300

    First commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import 'express-async-errors';
+import router from './routers/index';
+import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware"
+
+dotenv.config();
+const app = express();
+app.use(cors(), express.json());
+app.use(router);
+app.use(errorHandlerMiddleware);
+
+export default app;
